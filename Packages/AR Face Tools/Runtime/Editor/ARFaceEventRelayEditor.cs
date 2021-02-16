aa0a8acda09171f62fefdfb6d1cab930a3ac781c
Edwon <edwonia@gmail.com>
2021-02-16 20:02:40 +0900

    made editor of face relay easier to deal with

--- Packages/AR Face Tools/Runtime/Editor/ARFaceEventRelayEditor.cs
+++ Packages/AR Face Tools/Runtime/Editor/ARFaceEventRelayEditor.cs
@@ -17,7 +17,14 @@ namespace Edwon.ARFaceTools
 	[CustomEditor(typeof(ARFaceEventRelay))]
 	public class ARFaceEventRelayInspector : Editor
 	{
-		private bool showUnusedEvents;
+		private bool showUnusedEvents = false;
+        private bool showEyeEvents = true;
+        private bool showCheekEvents = true;
+        private bool showBrowEvents = true;
+        private bool showJawEvents = true;
+        private bool showMouthEvents = true;
+        private bool showOtherEvents = true;
+
         ARFaceEventRelay script;
 
         void OnEnable()
@@ -30,62 +37,87 @@ namespace Edwon.ARFaceTools
 		{
 			EditorGUILayout.Separator();
 
-			showUnusedEvents = EditorGUILayout.Foldout(showUnusedEvents, "Show Unused Events");
-
+            showEyeEvents = EditorGUILayout.Toggle("Show Eye Events", showEyeEvents);
+            showCheekEvents = EditorGUILayout.Toggle("Show Cheek Events", showCheekEvents);
+            showBrowEvents = EditorGUILayout.Toggle("Show Brow Events", showBrowEvents);
+            showJawEvents = EditorGUILayout.Toggle("Show Jaw Events", showJawEvents);
+            showMouthEvents = EditorGUILayout.Toggle("Show Mouth Events", showMouthEvents);
+            showOtherEvents = EditorGUILayout.Toggle("Show Other Events", showOtherEvents);
+            
+			showUnusedEvents = EditorGUILayout.Foldout(showUnusedEvents, "Unused Events");
+        
 			EditorGUILayout.Separator();
 
-            DrawIfEventUsed(script.browDownLeftEvent, "browDownLeftEvent");
-            DrawIfEventUsed(script.browDownRightEvent, "browDownRightEvent");
-            DrawIfEventUsed(script.browInnerUpEvent, "browInnerUpEvent");
-            DrawIfEventUsed(script.browOuterUpLeftEvent, "browOuterUpLeftEvent");
-            DrawIfEventUsed(script.browOuterUpRightEvent, "browOuterUpRightEvent");
-            DrawIfEventUsed(script.cheekPuffEvent, "cheekPuffEvent");
-            DrawIfEventUsed(script.cheekSquintLeftEvent, "cheekSquintLeftEvent");
-            DrawIfEventUsed(script.cheekSquintRightEvent, "cheekSquintRightEvent");
-            DrawIfEventUsed(script.eyeBlinkLeftEvent, "eyeBlinkLeftEvent");
-            DrawIfEventUsed(script.eyeBlinkRightEvent, "eyeBlinkRightEvent");
-            DrawIfEventUsed(script.eyeLookDownLeftEvent, "eyeLookDownLeftEvent");
-            DrawIfEventUsed(script.eyeLookDownRightEvent, "eyeLookDownRightEvent");
-            DrawIfEventUsed(script.eyeLookInLeftEvent, "eyeLookInLeftEvent");
-            DrawIfEventUsed(script.eyeLookInRightEvent, "eyeLookInRightEvent");
-            DrawIfEventUsed(script.eyeLookOutLeftEvent, "eyeLookOutLeftEvent");
-            DrawIfEventUsed(script.eyeLookOutRightEvent, "eyeLookOutRightEvent");
-            DrawIfEventUsed(script.eyeLookUpLeftEvent, "eyeLookUpLeftEvent");
-            DrawIfEventUsed(script.eyeLookUpRightEvent, "eyeLookUpRightEvent");
-            DrawIfEventUsed(script.eyeSquintLeftEvent, "eyeSquintLeftEvent");
-            DrawIfEventUsed(script.eyeSquintRightEvent, "eyeSquintRightEvent");
-            DrawIfEventUsed(script.eyeWideLeftEvent, "eyeWideLeftEvent");
-            DrawIfEventUsed(script.eyeWideRightEvent, "eyeWideRightEvent");
-            DrawIfEventUsed(script.jawForwardEvent, "jawForwardEvent");
-            DrawIfEventUsed(script.jawLeftEvent, "jawLeftEvent");
-            DrawIfEventUsed(script.jawOpenEvent, "jawOpenEvent");
-            DrawIfEventUsed(script.jawRightEvent, "jawRightEvent");
-            DrawIfEventUsed(script.mouthCloseEvent, "mouthCloseEvent");
-            DrawIfEventUsed(script.mouthDimpleLeftEvent, "mouthDimpleLeftEvent");
-            DrawIfEventUsed(script.mouthDimpleRightEvent, "mouthDimpleRightEvent");
-            DrawIfEventUsed(script.mouthFrownLeftEvent, "mouthFrownLeftEvent");
-            DrawIfEventUsed(script.mouthFrownRightEvent, "mouthFrownRightEvent");
-            DrawIfEventUsed(script.mouthFunnelEvent, "mouthFunnelEvent");
-            DrawIfEventUsed(script.mouthLeftEvent, "mouthLeftEvent");
-            DrawIfEventUsed(script.mouthLowerDownLeftEvent, "mouthLowerDownLeftEvent");
-            DrawIfEventUsed(script.mouthLowerDownRightEvent, "mouthLowerDownRightEvent");
-            DrawIfEventUsed(script.mouthPressLeftEvent, "mouthPressLeftEvent");
-            DrawIfEventUsed(script.mouthPressRightEvent, "mouthPressRightEvent");
-            DrawIfEventUsed(script.mouthPuckerEvent, "mouthPuckerEvent");
-            DrawIfEventUsed(script.mouthRightEvent, "mouthRightEvent");
-            DrawIfEventUsed(script.mouthRollLowerEvent, "mouthRollLowerEvent");
-            DrawIfEventUsed(script.mouthRollUpperEvent, "mouthRollUpperEvent");
-            DrawIfEventUsed(script.mouthShrugLowerEvent, "mouthShrugLowerEvent");
-            DrawIfEventUsed(script.mouthShrugUpperEvent, "mouthShrugUpperEvent");
-            DrawIfEventUsed(script.mouthSmileLeftEvent, "mouthSmileLeftEvent");
-            DrawIfEventUsed(script.mouthSmileRightEvent, "mouthSmileRightEvent");
-            DrawIfEventUsed(script.mouthStretchLeftEvent, "mouthStretchLeftEvent");
-            DrawIfEventUsed(script.mouthStretchRightEvent, "mouthStretchRightEvent");
-            DrawIfEventUsed(script.mouthUpperUpLeftEvent, "mouthUpperUpLeftEvent");
-            DrawIfEventUsed(script.mouthUpperUpRightEvent, "mouthUpperUpRightEvent");
-            DrawIfEventUsed(script.noseSneerLeftEvent, "noseSneerLeftEvent");
-            DrawIfEventUsed(script.noseSneerRightEvent, "noseSneerRightEvent");
-            DrawIfEventUsed(script.tongueOutEvent, "tongueOutEvent");
+            if (showBrowEvents)
+            {
+                DrawIfEventUsed(script.browDownLeftEvent, "browDownLeftEvent");
+                DrawIfEventUsed(script.browDownRightEvent, "browDownRightEvent");
+                DrawIfEventUsed(script.browInnerUpEvent, "browInnerUpEvent");
+                DrawIfEventUsed(script.browOuterUpLeftEvent, "browOuterUpLeftEvent");
+                DrawIfEventUsed(script.browOuterUpRightEvent, "browOuterUpRightEvent");
+            }
+            if (showCheekEvents)
+            {
+                DrawIfEventUsed(script.cheekPuffEvent, "cheekPuffEvent");
+                DrawIfEventUsed(script.cheekSquintLeftEvent, "cheekSquintLeftEvent");
+                DrawIfEventUsed(script.cheekSquintRightEvent, "cheekSquintRightEvent");
+            }
+            if (showEyeEvents)
+            {
+                DrawIfEventUsed(script.eyeBlinkLeftEvent, "eyeBlinkLeftEvent");
+                DrawIfEventUsed(script.eyeBlinkRightEvent, "eyeBlinkRightEvent");
+                DrawIfEventUsed(script.eyeLookDownLeftEvent, "eyeLookDownLeftEvent");
+                DrawIfEventUsed(script.eyeLookDownRightEvent, "eyeLookDownRightEvent");
+                DrawIfEventUsed(script.eyeLookInLeftEvent, "eyeLookInLeftEvent");
+                DrawIfEventUsed(script.eyeLookInRightEvent, "eyeLookInRightEvent");
+                DrawIfEventUsed(script.eyeLookOutLeftEvent, "eyeLookOutLeftEvent");
+                DrawIfEventUsed(script.eyeLookOutRightEvent, "eyeLookOutRightEvent");
+                DrawIfEventUsed(script.eyeLookUpLeftEvent, "eyeLookUpLeftEvent");
+                DrawIfEventUsed(script.eyeLookUpRightEvent, "eyeLookUpRightEvent");
+                DrawIfEventUsed(script.eyeSquintLeftEvent, "eyeSquintLeftEvent");
+                DrawIfEventUsed(script.eyeSquintRightEvent, "eyeSquintRightEvent");
+                DrawIfEventUsed(script.eyeWideLeftEvent, "eyeWideLeftEvent");
+                DrawIfEventUsed(script.eyeWideRightEvent, "eyeWideRightEvent");
+            }
+            if (showJawEvents)
+            {
+                DrawIfEventUsed(script.jawForwardEvent, "jawForwardEvent");
+                DrawIfEventUsed(script.jawLeftEvent, "jawLeftEvent");
+                DrawIfEventUsed(script.jawOpenEvent, "jawOpenEvent");
+                DrawIfEventUsed(script.jawRightEvent, "jawRightEvent");
+            }
+            if (showMouthEvents)
+            {
+                DrawIfEventUsed(script.mouthCloseEvent, "mouthCloseEvent");
+                DrawIfEventUsed(script.mouthDimpleLeftEvent, "mouthDimpleLeftEvent");
+                DrawIfEventUsed(script.mouthDimpleRightEvent, "mouthDimpleRightEvent");
+                DrawIfEventUsed(script.mouthFrownLeftEvent, "mouthFrownLeftEvent");
+                DrawIfEventUsed(script.mouthFrownRightEvent, "mouthFrownRightEvent");
+                DrawIfEventUsed(script.mouthFunnelEvent, "mouthFunnelEvent");
+                DrawIfEventUsed(script.mouthLeftEvent, "mouthLeftEvent");
+                DrawIfEventUsed(script.mouthLowerDownLeftEvent, "mouthLowerDownLeftEvent");
+                DrawIfEventUsed(script.mouthLowerDownRightEvent, "mouthLowerDownRightEvent");
+                DrawIfEventUsed(script.mouthPressLeftEvent, "mouthPressLeftEvent");
+                DrawIfEventUsed(script.mouthPressRightEvent, "mouthPressRightEvent");
+                DrawIfEventUsed(script.mouthPuckerEvent, "mouthPuckerEvent");
+                DrawIfEventUsed(script.mouthRightEvent, "mouthRightEvent");
+                DrawIfEventUsed(script.mouthRollLowerEvent, "mouthRollLowerEvent");
+                DrawIfEventUsed(script.mouthRollUpperEvent, "mouthRollUpperEvent");
+                DrawIfEventUsed(script.mouthShrugLowerEvent, "mouthShrugLowerEvent");
+                DrawIfEventUsed(script.mouthShrugUpperEvent, "mouthShrugUpperEvent");
+                DrawIfEventUsed(script.mouthSmileLeftEvent, "mouthSmileLeftEvent");
+                DrawIfEventUsed(script.mouthSmileRightEvent, "mouthSmileRightEvent");
+                DrawIfEventUsed(script.mouthStretchLeftEvent, "mouthStretchLeftEvent");
+                DrawIfEventUsed(script.mouthStretchRightEvent, "mouthStretchRightEvent");
+                DrawIfEventUsed(script.mouthUpperUpLeftEvent, "mouthUpperUpLeftEvent");
+                DrawIfEventUsed(script.mouthUpperUpRightEvent, "mouthUpperUpRightEvent");
+            }
+            if (showOtherEvents)
+            {
+                DrawIfEventUsed(script.noseSneerLeftEvent, "noseSneerLeftEvent");
+                DrawIfEventUsed(script.noseSneerRightEvent, "noseSneerRightEvent");
+                DrawIfEventUsed(script.tongueOutEvent, "tongueOutEvent");
+            }
 
             serializedObject.ApplyModifiedProperties();
 		}
